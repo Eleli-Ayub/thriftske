e6e58b3e0880fb2d2f39dfbf369dbc0793c18bba
Eleli-Ayub <lesosioayub@gmail.com>
2023-06-03 00:13:23 +0300

    done

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@material-tailwind/react";
+import Link from "next/link";
 
 const SITEMAP = [
   {
@@ -43,12 +44,12 @@ export default function Footer() {
                     color="blue-gray"
                     className="font-normal"
                   >
-                    <a
+                    <Link
                       href="#"
                       className="inline-block py-1 pr-2 transition-transform hover:scale-105"
                     >
                       {link}
-                    </a>
+                    </Link>
                   </Typography>
                 ))}
               </ul>
@@ -61,7 +62,7 @@ export default function Footer() {
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
             &copy; {currentYear}{" "}
-            <a href="https://material-tailwind.com/">Eleli Afrika</a>. All
+            <Link href="https://material-tailwind.com/">Eleli Afrika</Link>. All
             Rights Reserved.
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
