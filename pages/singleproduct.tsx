990aee2e4ffa3aa2a3c181077ce5799994dcabf9
Eleli-Ayub <lesosioayub@gmail.com>
2023-06-03 11:13:43 +0300

    made single page resposive

--- pages/singleproduct.tsx
+++ pages/singleproduct.tsx
@@ -6,8 +6,8 @@ import Productcard from "@/components/global/Productcard";
 function singleproduct() {
   return (
     <div className="flex flex-col">
-      <div className=" h-screen max-h-screen px-10 pt-10 flex flex-row gap-10 font-comfortaa ">
-        <div className="h-5/6 w-2/3 ">
+      <div className=" h-fit lg:h-screen lg:max-h-screen px-3 lg:px-10 pt-10 flex flex-col lg:flex-row gap:5 lg:gap-10 font-comfortaa ">
+        <div className="h-5/6 w-full lg:w-2/3 ">
           <Image
             className="h-full w-full rounded-2xl object-cover "
             src={productImage}
@@ -16,30 +16,30 @@ function singleproduct() {
             alt="Product Image"
           />
         </div>
-        <section className="flex flex-col gap-10 w-1/3 pt-10 pl-10">
+        <section className="flex flex-col gap-5 lg:gap-10 lg:w-1/3 pt-5 lg:pt-10 lg:pl-10">
           <div>
             <nav className="flex flex-row gap-20 font-bold text-lg ">
               <h1>Official Suit</h1>
               <h1>Ksh. 12,000</h1>
             </nav>
-            <p className="w-2/3 mt-2">
+            <p className="text-sm lg:text-md w-full lg:w-2/3 mt-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, accusantium.
             </p>
           </div>
           <div>
             <h1 className=" font-bold mb-1">Colors</h1>
-            <div className="flex flex-row gap-1 child:border child:flex child:items-center child:justify-center child:border-stone-600 child:rounded-lg">
-              <div className="bg-red-800 h-[50px] w-[70px]">red</div>
-              <div className="bg-black h-[50px] w-[70px]">black</div>
-              <div className="bg-white h-[50px] w-[70px]">white</div>
-              <div className="bg-green-500 h-[50px] w-[70px]">green</div>
-              <div className="bg-blue-600 h-[50px] w-[70px]">blue</div>
+            <div className="flex child:text-sm flex-row gap-1 child:h-[40px] child:w-[60px] lg:child:h-[50px] lg:child:w-[70px] child:border child:flex child:items-center child:justify-center child:border-stone-600 child:rounded-md lg:child:rounded-lg">
+              <div className="bg-red-800">red</div>
+              <div className="bg-black">black</div>
+              <div className="bg-white">white</div>
+              <div className="bg-green-500">green</div>
+              <div className="bg-blue-600">blue</div>
             </div>
           </div>
           <div>
             <h1 className=" font-bold mb-1">Sizes</h1>
-            <div className="flex flex-row gap-1 child:border child:flex child:items-center child:justify-center child:border-stone-600 child:rounded-lg">
+            <div className="flex flex-row gap-1 text-sm lg:text-md child:border child:flex child:items-center child:justify-center child:border-stone-600 child:rounded-lg">
               <h1>32,</h1>
               <h1>35,</h1>
               <h1>37,</h1>
@@ -103,7 +103,7 @@ function singleproduct() {
         </h1>
         <span className="bg-gray-200 h-1 w-full"></span>
       </div>
-      <div className="px-5 flex flex-row flex-wrap">
+      <div className="px-2 lg:px-5 flex flex-row flex-wrap">
         <Productcard price={"21,000"} title={"Official Suit Black"} />
         <Productcard price={"21,000"} title={"Official Suit Black"} />
         <Productcard price={"21,000"} title={"Official Suit Black"} />
